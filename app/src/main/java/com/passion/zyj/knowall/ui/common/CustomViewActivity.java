3948c71d12d4b58ecdfcf472cafbc820d11ef2b4
zhaoyj <1650432983@qq.com>
2019-07-03 17:05:32 +0800

    增加学习文件

--- app/src/main/java/com/passion/zyj/knowall/ui/common/CustomViewActivity.java
+++ app/src/main/java/com/passion/zyj/knowall/ui/common/CustomViewActivity.java
@@ -1,9 +1,13 @@
 package com.passion.zyj.knowall.ui.common;
 
+import android.os.Handler;
+
 import com.passion.zyj.knowall.R;
 import com.passion.zyj.knowall.mvp.activity.BaseNorActivity;
 
 public class CustomViewActivity extends BaseNorActivity {
+
+    Handler handler = new Handler();
 
     @Override
     protected int getLayoutId() {
@@ -17,6 +21,24 @@ public class CustomViewActivity extends BaseNorActivity {
 
     @Override
     protected void initEventAndData() {
-
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                while (true) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            showToast("hello");
+                        }
+                    });
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 }
